aae66667d4afd46bff03ea440fd932f1c4e79eb9
kg123represent <30854288+kg123represent@users.noreply.github.com>
2017-09-15 13:07:02 +0530

    unknown materialize

--- js/materialize.js
+++ js/materialize.js
@@ -10019,3 +10019,7 @@ if (Vel) {
     $.error('Method ' + methodOrOptions + ' does not exist on jQuery.tap-target');
   };
 })(jQuery);
+
+ $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
